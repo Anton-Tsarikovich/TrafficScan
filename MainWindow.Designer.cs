0bf01c5c6fe5605ad9ad723906b45b0c91f0f33b
Antoshka <Anton.tsarikovich@gmail.com>
2016-07-07 17:32:55 +0300

    Release

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,253 @@
+﻿    namespace iipyLab7
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.startButton = new System.Windows.Forms.Button();
+            this.textLabel1 = new System.Windows.Forms.Label();
+            this.textLabel2 = new System.Windows.Forms.Label();
+            this.textLabel3 = new System.Windows.Forms.Label();
+            this.startMeterage = new System.Windows.Forms.Label();
+            this.finishMeterage = new System.Windows.Forms.Label();
+            this.workTime = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.trafficDiagramm = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trafficCount = new System.Windows.Forms.Label();
+            this.saveGraphic = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.totalTraffic = new System.Windows.Forms.Label();
+            this.toTray = new System.Windows.Forms.Button();
+            this.notifySniffer = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trafficDiagramm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(15, 383);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(107, 35);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Старт";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButtonOnClick);
+            // 
+            // textLabel1
+            // 
+            this.textLabel1.AutoSize = true;
+            this.textLabel1.Location = new System.Drawing.Point(9, 9);
+            this.textLabel1.Name = "textLabel1";
+            this.textLabel1.Size = new System.Drawing.Size(103, 13);
+            this.textLabel1.TabIndex = 1;
+            this.textLabel1.Text = "Начало измерения";
+            // 
+            // textLabel2
+            // 
+            this.textLabel2.AutoSize = true;
+            this.textLabel2.Location = new System.Drawing.Point(10, 31);
+            this.textLabel2.Name = "textLabel2";
+            this.textLabel2.Size = new System.Drawing.Size(97, 13);
+            this.textLabel2.TabIndex = 2;
+            this.textLabel2.Text = "Конец измерения";
+            // 
+            // textLabel3
+            // 
+            this.textLabel3.AutoSize = true;
+            this.textLabel3.Location = new System.Drawing.Point(10, 53);
+            this.textLabel3.Name = "textLabel3";
+            this.textLabel3.Size = new System.Drawing.Size(99, 13);
+            this.textLabel3.TabIndex = 3;
+            this.textLabel3.Text = "Время измерения";
+            // 
+            // startMeterage
+            // 
+            this.startMeterage.AutoSize = true;
+            this.startMeterage.Location = new System.Drawing.Point(190, 9);
+            this.startMeterage.Name = "startMeterage";
+            this.startMeterage.Size = new System.Drawing.Size(0, 13);
+            this.startMeterage.TabIndex = 4;
+            // 
+            // finishMeterage
+            // 
+            this.finishMeterage.AutoSize = true;
+            this.finishMeterage.Location = new System.Drawing.Point(190, 31);
+            this.finishMeterage.Name = "finishMeterage";
+            this.finishMeterage.Size = new System.Drawing.Size(0, 13);
+            this.finishMeterage.TabIndex = 5;
+            // 
+            // workTime
+            // 
+            this.workTime.AutoSize = true;
+            this.workTime.Location = new System.Drawing.Point(190, 53);
+            this.workTime.Name = "workTime";
+            this.workTime.Size = new System.Drawing.Size(0, 13);
+            this.workTime.TabIndex = 6;
+            // 
+            // trafficDiagramm
+            // 
+            chartArea1.AxisX.ScaleBreakStyle.Enabled = true;
+            chartArea1.AxisY.Maximum = 1000D;
+            chartArea1.AxisY.ScaleView.SmallScrollMinSize = 20D;
+            chartArea1.BorderColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.trafficDiagramm.ChartAreas.Add(chartArea1);
+            this.trafficDiagramm.Location = new System.Drawing.Point(307, 31);
+            this.trafficDiagramm.Name = "trafficDiagramm";
+            this.trafficDiagramm.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.trafficDiagramm.Series.Add(series1);
+            this.trafficDiagramm.Size = new System.Drawing.Size(491, 376);
+            this.trafficDiagramm.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Текущий трафик";
+            // 
+            // trafficCount
+            // 
+            this.trafficCount.AutoSize = true;
+            this.trafficCount.Location = new System.Drawing.Point(190, 74);
+            this.trafficCount.Name = "trafficCount";
+            this.trafficCount.Size = new System.Drawing.Size(0, 13);
+            this.trafficCount.TabIndex = 9;
+            // 
+            // saveGraphic
+            // 
+            this.saveGraphic.Location = new System.Drawing.Point(15, 321);
+            this.saveGraphic.Name = "saveGraphic";
+            this.saveGraphic.Size = new System.Drawing.Size(107, 40);
+            this.saveGraphic.TabIndex = 21;
+            this.saveGraphic.Text = "Сохранить график";
+            this.saveGraphic.UseVisualStyleBackColor = true;
+            this.saveGraphic.Click += new System.EventHandler(this.saveGraphic_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(15, 261);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(107, 39);
+            this.clearButton.TabIndex = 22;
+            this.clearButton.Text = "Очистить график";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Общий трафик";
+            // 
+            // totalTraffic
+            // 
+            this.totalTraffic.AutoSize = true;
+            this.totalTraffic.Location = new System.Drawing.Point(190, 98);
+            this.totalTraffic.Name = "totalTraffic";
+            this.totalTraffic.Size = new System.Drawing.Size(0, 13);
+            this.totalTraffic.TabIndex = 24;
+            // 
+            // toTray
+            // 
+            this.toTray.Location = new System.Drawing.Point(16, 205);
+            this.toTray.Name = "toTray";
+            this.toTray.Size = new System.Drawing.Size(106, 39);
+            this.toTray.TabIndex = 25;
+            this.toTray.Text = "Свернуть в трей";
+            this.toTray.UseVisualStyleBackColor = true;
+            this.toTray.Click += new System.EventHandler(this.toTray_Click);
+            // 
+            // notifySniffer
+            // 
+            this.notifySniffer.Icon = ((System.Drawing.Icon)(resources.GetObject("notifySniffer.Icon")));
+            this.notifySniffer.Text = "Sniffer";
+            this.notifySniffer.Visible = true;
+            this.notifySniffer.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifySniffer_MouseDoubleClick);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 444);
+            this.Controls.Add(this.toTray);
+            this.Controls.Add(this.totalTraffic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.saveGraphic);
+            this.Controls.Add(this.trafficCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trafficDiagramm);
+            this.Controls.Add(this.workTime);
+            this.Controls.Add(this.finishMeterage);
+            this.Controls.Add(this.startMeterage);
+            this.Controls.Add(this.textLabel3);
+            this.Controls.Add(this.textLabel2);
+            this.Controls.Add(this.textLabel1);
+            this.Controls.Add(this.startButton);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trafficDiagramm)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label textLabel1;
+        private System.Windows.Forms.Label textLabel2;
+        private System.Windows.Forms.Label textLabel3;
+        private System.Windows.Forms.Label startMeterage;
+        private System.Windows.Forms.Label finishMeterage;
+        private System.Windows.Forms.Label workTime;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart trafficDiagramm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label trafficCount;
+        private System.Windows.Forms.Button saveGraphic;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label totalTraffic;
+        private System.Windows.Forms.Button toTray;
+        private System.Windows.Forms.NotifyIcon notifySniffer;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace iipyLab7
+{
+    public enum Protocol
+    {
+        TCP = 6,
+        UDP = 17,
+        Unknown = -1
+    };
+    public partial class MainWindow : Form
+    {
+        private const double maxTraffic = 1;
+        private bool buttonPressed = false;
+        private DateTime startTime;
+        private DateTime stopTime;
+        private TimeSpan workingTime;
+
+        private Timer timer;
+        private Socket mainSocket;
+        private byte[] byteData = new byte[4096];
+        private double ipLenght = 0;
+        private double tcpLenght = 0;
+        private double secondLength = 0;
+        private double totalLenght = 0;
+        private uint tick = 0;
+        private string postfixTotal = "bit";
+        private string postfixLocal = "bit";
+
+        private delegate void AddBytes();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startButtonOnClick(object sender, EventArgs e)
+        {
+            if(!buttonPressed)
+            {
+                
+                startButton.Text = "&Стоп";
+                saveGraphic.Enabled = false;
+                clearButton.Enabled = false;
+                buttonPressed = true;
+                startTime = DateTime.Now;
+                startMeterage.Text = startTime.ToLongTimeString();
+              
+                setDiagramm();
+                prepareSocket();
+            }
+            else
+            {
+                tick = 0;
+                startButton.Text = "&Старт";
+                timer.Enabled = false;
+                startButton.Enabled = false;
+                buttonPressed = false;
+                saveGraphic.Enabled = true;
+                clearButton.Enabled = true;
+                stopTime = DateTime.Now;
+                finishMeterage.Text = stopTime.ToLongTimeString();
+                workingTime = stopTime - startTime;
+                workTime.Text = workingTime.Hours.ToString() + ":" + workingTime.Minutes.ToString() + ":" + workingTime.Seconds.ToString();
+                mainSocket.Close();
+            }
+        }
+
+        private void prepareSocket()
+        { 
+            mainSocket = new Socket(AddressFamily.InterNetwork,
+                      SocketType.Raw, ProtocolType.IP);
+            IPHostEntry ipEntry = Dns.GetHostByName(Dns.GetHostName());
+            IPAddress[] adr = ipEntry.AddressList;
+            Console.WriteLine(adr[0].ToString());
+            mainSocket.Bind(new IPEndPoint(IPAddress.Parse(adr[0].ToString()), 0));
+
+
+            mainSocket.SetSocketOption(SocketOptionLevel.IP,
+                                       SocketOptionName.HeaderIncluded,
+                                       true);
+
+            byte[] byTrue = new byte[4] { 1, 0, 0, 0 };
+            byte[] byOut = new byte[4] { 1, 0, 0, 0 };
+
+            mainSocket.IOControl(IOControlCode.ReceiveAll,
+                                 byTrue,
+                                 byOut);
+
+            mainSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None,
+                new AsyncCallback(OnReceive), null);
+        }
+        private void OnReceive(IAsyncResult ar)
+        {
+            try
+            {
+                int nReceived = mainSocket.EndReceive(ar);
+
+                ParseData(byteData, nReceived);
+
+                if (buttonPressed)
+                {
+                    byteData = new byte[4096];
+
+                    mainSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None,
+                        new AsyncCallback(OnReceive), null);
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        private void ParseData(byte[] byteData, int nReceived)
+        {
+            IPheader ipHeader = new IPheader(byteData, nReceived);
+            ipLenght = double.Parse(ipHeader.TotalLength);
+            switch (ipHeader.ProtocolType)
+            {
+                case Protocol.TCP:
+                    TCPheader tcpHeader = new TCPheader(ipHeader.Data, ipHeader.MessageLength);
+                    tcpLenght += double.Parse(tcpHeader.HeaderLength);
+                    break;
+               
+                case Protocol.Unknown:
+                    break;
+            }
+           
+            AddBytes addBytes = new AddBytes(onShowBytes);
+            workTime.Invoke(addBytes);
+        }
+
+        private void onShowBytes()
+        {
+            secondLength += ipLenght + tcpLenght;
+            if (postfixTotal.Equals("bit"))
+                totalLenght += secondLength;
+            else if (postfixTotal.Equals("Kbit"))
+                totalLenght += secondLength / 1000;
+            else if (postfixTotal.Equals("Mbit"))
+                totalLenght += secondLength / 1E6;
+        }
+
+        private void setDiagramm()
+        {
+
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += timerTick;
+            timer.Enabled = true;
+            trafficDiagramm.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            trafficDiagramm.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            trafficDiagramm.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+
+        }
+        private void timerTick(object sender, EventArgs e)
+        {
+            tick++;
+
+            if(tick <= 15)
+            {
+                trafficDiagramm.ChartAreas[0].AxisX.Maximum = 15;
+            }
+            if(tick > 15)
+            {
+                trafficDiagramm.ChartAreas[0].AxisX.Maximum = tick;
+                trafficDiagramm.ChartAreas[0].AxisX.Minimum = tick - 15;
+            }
+            
+
+            if (secondLength >= 1000 && secondLength < 1E6)
+            {
+                secondLength = secondLength / 1000;
+                postfixLocal = "Kbit";
+            }
+            if(secondLength >= 1E6)
+            {
+                secondLength = secondLength / 1E6;
+                postfixLocal = "Mbit";
+            }
+            
+            trafficCount.Text = secondLength.ToString("0.00") + postfixLocal;
+            trafficDiagramm.ChartAreas.FindByName("ChartArea1").AxisY.Title = postfixLocal;
+            
+            trafficDiagramm.Series[0].Points.AddXY(tick, secondLength);
+            if (WindowState == FormWindowState.Minimized && secondLength >= maxTraffic && postfixLocal.Equals("Kbit") || postfixLocal.Equals("Mbit"))
+            {
+                notifySniffer.BalloonTipTitle = "Трафик превысил норму";
+                notifySniffer.BalloonTipText = "Значение текущего трафика за секунду = " + secondLength.ToString() + " " + postfixLocal;
+                notifySniffer.ShowBalloonTip(2000);
+            }
+            
+            if (totalLenght >= 1000 && totalLenght < 1000000 && postfixTotal.Equals("bit"))
+            {
+                totalLenght /= 1000;
+                postfixTotal = "Kbit";
+            }
+            if (totalLenght >= 1000 && postfixTotal.Equals("Kbit"))
+            {
+                totalLenght /= 1000;
+                postfixTotal = "Mbit";
+            }
+            totalTraffic.Text = totalLenght.ToString("0.00") + postfixTotal;
+            secondLength = 0;
+            tcpLenght = 0;
+            ipLenght = 0;
+            postfixLocal = "bit";
+
+
+        }
+
+        private void saveGraphic_Click(object sender, EventArgs e)
+        {
+            string tempTimeString = startTime.ToLongTimeString();
+            trafficDiagramm.SaveImage("C:\\graphic/" + tempTimeString.Replace(":", "_") + ".png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png);
+            saveGraphic.Enabled = false;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            trafficDiagramm.Series[0].Points.Clear();
+            startButton.Enabled = true;
+            secondLength = 0;
+            totalLenght = 0;
+            workTime.Text = "";
+            totalTraffic.Text = "";
+            finishMeterage.Text = "";
+            startMeterage.Text = "";
+            trafficCount.Text = "";
+            postfixTotal = "bit";
+            postfixLocal = "bit";
+            trafficDiagramm.ChartAreas[0].AxisX.Maximum = 15;
+            trafficDiagramm.ChartAreas[0].AxisX.Minimum = 0;
+            clearButton.Enabled = false;
+        }
+
+        private void toTray_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+            Hide();
+        }
+
+        private void notifySniffer_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+    }
+}
+
